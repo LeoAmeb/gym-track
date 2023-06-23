cd4f7d688831d873b1022d1cb97c6f368ab7470d
LeoAmeb <leoameb@gmail.com>
2023-06-23 16:30:32 -0600

    creación y configuración de modelo de usuarios

---
+++ apps/usuarios/models.py
@@ -0,0 +1,26 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+# Create your models here.
+
+
+class Usuario(AbstractUser):
+    """Modelo de usuario extendido para usuarios personalizados."""
+    SEXO_CHOICES = (
+        ('M', 'Masculino'),
+        ('F', 'Femenino'),
+        ('O', 'Otro'),
+    )
+
+    peso = models.FloatField()
+    altura = models.FloatField()
+    fecha_nacimiento = models.DateField(blank=True, null=True)
+    foto = models.FileField(upload_to='usuarios/', blank=True, null=True)
+    sexo = models.CharField(max_length=1)
+    sexo_otro = models.CharField(max_length=50, blank=True, null=True)
+
+    REQUIRED_FIELDS = ['first_name', 'last_name', 'sexo', 'peso', 'altura']
+
+    def __str__(self):
+        """Return username."""
+        return self.username
--- gymtrack/settings.py
+++ gymtrack/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # Third apps
+    'rest_framework',
+    # Own apps
+    'apps.usuarios',
 ]
 
 MIDDLEWARE = [
@@ -99,6 +103,7 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+AUTH_USER_MODEL = 'usuarios.Usuario'
 
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
