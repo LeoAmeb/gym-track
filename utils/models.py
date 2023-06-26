a9fa4f0c351badea50dd24f4a308e95833aefc6e
LeoAmeb <leoameb@gmail.com>
2023-06-26 12:52:23 -0600

    Se crea manager custom

--- utils/models.py
+++ utils/models.py
@@ -1,4 +1,43 @@
+from django.utils.timezone import now
 from django.db import models
+
+
+class DeactivateQuerySet(models.query.QuerySet):
+    '''
+    QuerySet whose delete() does not delete items, but instead marks the
+    rows as not active, and updates the timestamps
+    '''
+
+    def delete(self):
+        self.desactivar()
+
+    def desactivar(self):
+        return self.update(activo=False, fecha_edicion=now())
+
+    def activar(self):
+        return self.update(activo=True, fecha_edicion=now())
+
+    def activos(self):
+        return self.filter(activo=True)
+
+    def inactivos(self):
+        return self.filter(activo=False)
+
+
+class DeactivateManager(models.Manager):
+    '''
+    Manager that returns a DeactivateQuerySet,
+    to prevent object deletion.
+    '''
+
+    def get_query_set(self):
+        return DeactivateQuerySet(self.model, using=self._db)
+
+    def activos(self):
+        return self.get_query_set().activos()
+
+    def inactivos(self):
+        return self.get_query_set().inactivos()
 
 
 class BaseModel(models.Model):
@@ -10,6 +49,8 @@ class BaseModel(models.Model):
     fecha_creacion = models.DateTimeField(auto_now_add=True)
     fecha_edicion = models.DateTimeField(auto_now=True)
     activo = models.BooleanField(default=True)
+
+    objects = DeactivateManager()
 
     class Meta:
         abstract = True
